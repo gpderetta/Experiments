a25e8f6b1657ea0ab01784c5a1201b8827444fc2
gpd <gpd@imladris.(none)>
2009-07-20 04:18:43 +0200

    now with alpha

--- color.hpp
+++ color.hpp
@@ -4,11 +4,13 @@ namespace eb {
 struct color {
   color(double r,
         double g,
-        double b)
-   :r(r),g(g),b(b){}
+        double b,
+        double a = 1)
+   :r(r),g(g),b(b), a(a){}
   double r;
   double g;
   double b;
+  double a;
 };
 }
 #endif
