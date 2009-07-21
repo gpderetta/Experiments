f7b2272a52c463d03624919f6dfe4dd7454012d6
gpd <gpd@imladris.(none)>
2009-07-21 19:18:04 +0200

    gravitational physics (sort of)

--- geometry.hpp
+++ geometry.hpp
@@ -1,17 +1,34 @@
 #ifndef EB_GEOMETRY_HPP
 # define EB_GEOMETRY_HPP
 # include <cmath>
+# include <iostream>
+# include <cstring>
 namespace eb {
 
 typedef float real;
 typedef real angle;
+
+
+inline
+float __attribute__((pure))  inv_sqrt(float x) {
+  float xhalf = 0.5f * x;
+  int i;
+  std::memcpy((char*)&i, (char*)&x, sizeof(x));
+  i = 0x5f3759df - (i >> 1);
+  std::memcpy((char*)&x, (char*)&i, sizeof(x));
+  x = x*(1.5f - xhalf*x*x);
+//  x = x*(1.5f - xhalf*x*x);
+//  x = x*(1.5f - xhalf*x*x);
+  return x;
+}
 
 struct point;
 struct vector {
   real x, y, z;
 
   void test() const {}
-    
+
+  vector() : x(0), y(0), z(0) {}
   vector(real x, real y, real z = 0)
    : x(x),y(y),z(z) { test(); }
   vector& operator *=(real rhs) {
@@ -30,6 +47,18 @@ struct vector {
     return lhs *= rhs;
   } 
 
+  vector& operator -() {
+    x = -x;
+    y = -y;
+    z = -z;
+    test();
+    return *this;
+  }
+
+  friend std::ostream&operator<<(std::ostream&o, vector v) {
+    return o << "("<<v.x<<", "<<v.y<<")";
+  }
+  
   vector& operator /=(real rhs) {
     x /= rhs;
     y /= rhs;
@@ -75,6 +104,16 @@ struct vector {
   }
 
   friend
+  vector versor(vector v) {
+# ifdef EB_SLOW
+    return v /= norm_2(v);
+# else
+    return v *= inv_sqrt(inner_prod(v));
+# endif 
+  }
+  
+  
+  friend
   real inner_prod(vector lhs, vector rhs) {
     lhs.test();
     rhs.test();
@@ -83,6 +122,12 @@ struct vector {
             lhs.z*rhs.z);
   }
 
+  friend
+  vector rotate(vector v, float angle) {
+    return vector(v.x*cos(angle)-v.y*sin(angle),
+                  v.x*sin(angle)+v.y*cos(angle));
+  }
+  
   friend
   // return inner_prod with itself
   real inner_prod(vector v) {
@@ -96,9 +141,15 @@ struct vector {
 
 };
 
+
 struct point {
   real x, y, z;
 
+  friend std::ostream&operator<<(std::ostream&o, point v) {
+    return o << "("<<v.x<<", "<<v.y<<")";
+  }
+
+  
   void test(){}
     
   point(real x, real y, real z = 0) 
